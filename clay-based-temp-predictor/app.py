2cc9009868155889e1ce9cc66ef1986c6f2bc503
Parth Shukla <shuklaparth501@gmail.com>
2022-07-08 13:42:20 +0530

    Add files via upload

---
+++ clay-based-temp-predictor/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+
+
+pipe = pickle.load(open('phy-clay-temp.pkl','rb'))
+st.title('Clay Predictor')
+
+
+LatDegree = st.number_input('LatDegree')
+LongDegree = st.number_input('LongDegree')
+Clay = st.number_input('Clay')
+MeasureDepth_m = st.number_input('MeasureDepth_m')
+
+
+# 'pH(CaCl2)':[pH(CaCl2)],'pH(H2O)':[pH(H2O)],
+if st.button('Predict Clay'):
+      input=pd.DataFrame({'LatDegree':[LatDegree],'LongDegree':[LongDegree],'Clay':[Clay],'MeasureDepth_m':[MeasureDepth_m]})
+      result = pipe.predict(input)
+      st.success('THE Clay FOR GIVEN DATA WILL BE {}'.format(result))
+# st.header(result)
