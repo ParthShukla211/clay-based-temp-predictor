601186bc50c2f8ec7fce4a5ad6fd32d1f31b2e4d
Parth Shukla <shuklaparth501@gmail.com>
2022-08-04 08:05:55 +0530

    Update app.py

--- clay-based-temp-predictor/app.py
+++ clay-based-temp-predictor/app.py
@@ -16,7 +16,7 @@ MeasureDepth_m = st.number_input('MeasureDepth_m')
 
 
 # 'pH(CaCl2)':[pH(CaCl2)],'pH(H2O)':[pH(H2O)],
-if st.button('Predict Clay'):
+if st.button('Predict'):
       input=pd.DataFrame({'LatDegree':[LatDegree],'LongDegree':[LongDegree],'Clay':[Clay],'MeasureDepth_m':[MeasureDepth_m]})
       result = pipe.predict(input)
       st.success('THE TEMPERATURE FOR GIVEN DATA WILL BE {}'.format(result))
